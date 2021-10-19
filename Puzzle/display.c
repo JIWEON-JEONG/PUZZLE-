8e86d77ca5ead4f508afcfefd8dee45f449b8dff
jiweon <dlektl6@naver.com>
2021-10-19 18:57:10 +0900

    reduce code size

--- Puzzle/display.c
+++ Puzzle/display.c
@@ -10,77 +10,30 @@ extern int maxCount;
 void display() {
 	int i;
 	system("cls");
-	for (i = 0; i < 3; i++)
-	{
-		if (i == 0)
-		{
-			printf("忙式式成");
-		}
-		else if (i == 3 - 1)
-		{
-			printf("式式忖\n");
-		}
-		else
-		{
-			printf("式式成");
-		}
-	}
-
-	for (i = 0; i < 9; i++)
-	{
-		if (pz[i / 3][i % 3] == 0)
-		{
+	printf("忙式式成式式成式式忖\n");
+	for (i = 0; i < 9; i++) {
+		if (pz[i / 3][i % 3] == 0){
 			printf("弛 *");
 		}
 
-		else
-		{
+		else{
 			printf("弛%2d", pz[i / 3][i % 3]);
 		}
 
-		if (i % 3 == 2)
-		{
+		if (i % 3 == 2){
+
 			printf("弛\n");
 
-			if (i < 3 * (2))
+			if (i < 6)
 			{
-				for (int h = 0; h < 3; h++)
-				{
-					if (h == 0)
-					{
-						printf("戍式式托");
-					}
-					else if (h == 3 - 1)
-					{
-						printf("式式扣\n");
-					}
-					else
-					{
-						printf("式式托");
-					}
-				}
+				printf("戍式式托式式托式式扣\n");
 			}
-
-			else
-			{
-				for (int h = 0; h < 3; h++)
-				{
-					if (h == 0)
-					{
-						printf("戌式式扛");
-					}
-					else if (h == 3 - 1)
-					{
-						printf("式式戎\n");
-					}
-					else
-					{
-						printf("式式扛");
-					}
-				}
+			else{
+				printf("戌式式扛式式扛式式戎\n");
 			}
 		}
 	}
+
 	printf("\ncRow : %d\n", cRow);
 	printf("cColumn : %d\n", cColumn);
 	printf("=========================\n");
